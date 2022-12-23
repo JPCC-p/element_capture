884300cbee6a9d92f7701c831128a0dd316bc3cd
JPCC <120706915+JPCC-p@users.noreply.github.com>
2022-12-23 09:24:50 +0000

    Change: content.js

--- src/content.js
+++ src/content.js
@@ -2,6 +2,7 @@ chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         if (request.message == "Capture!") {
             document.getElementsByTagName("h1")[0].innerHTML = request.data
+            console.log(request.data)
         }
     }
 );
--- src/popup.js
+++ src/popup.js
@@ -6,9 +6,9 @@ function onRun() {
     chrome.storage.sync.get(null, (options) => {
         // test用背景変更
         document.body.style.backgroundColor = "#fee";
-        alert(options.Capture_Name)
+        // console.log(options.Capture_Name)
         //content.jsに送る
-        chrome.tab.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
             chrome.tabs.sendMessage(tabs[0].id, {
                 message: "Capture",
                 "data": options.Capture_Name

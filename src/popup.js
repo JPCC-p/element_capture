cd76fe5f32d48a6d4055becc64f1949cc03a4a3b
JPCC <120706915+JPCC-p@users.noreply.github.com>
2022-12-23 08:18:26 +0000

    Fix popup.js

--- src/popup.js
+++ src/popup.js
@@ -1,16 +1,13 @@
 document.getElementById("btn").addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-    chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        function: onRun,
-    });
+    onRun();
 });
 
 function onRun() {
     chrome.storage.sync.get(null, (options) => {
+        // test用背景変更
         document.body.style.backgroundColor = "#fee";
         alert(options.Capture_Name)
-
+        //content.jsに送る
         chrome.tab.query({ active: true, currentWindow: true }, function (tabs) {
             chrome.tabs.sendMessage(tabs[0].id, {
                 message: "Capture",

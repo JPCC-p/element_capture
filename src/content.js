ebeacf05e97905fac7e97afc5fc99de857eaa5e2
JPCC <120706915+JPCC-p@users.noreply.github.com>
2022-12-23 10:04:37 +0000

    modified: content.js

--- src/content.js
+++ src/content.js
@@ -1,6 +1,7 @@
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
-        if (request.message == "Capture!") {
+        if (request.message == "Capture") {
+            document.body.style.backgroundColor = "#fee";
             document.getElementsByTagName("h1")[0].innerHTML = request.data
             console.log(request.data)
         }

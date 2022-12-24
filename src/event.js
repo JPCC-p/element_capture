352011696d58b92e980972e73e1e7dc2fac2d477
JPCC <120706915+JPCC-p@users.noreply.github.com>
2022-12-24 13:15:19 +0000

    Fix: event.js

--- src/event.js
+++ src/event.js
@@ -6,21 +6,28 @@ chrome.runtime.onMessage.addListener(
 
         const rect = request.data.rect;
 
-        chrome.debugger.attach({ tabId: request.tabid }, "1.3", () => { console.log("Attach debugger") })
-        chrome.debugger.sendCommand(
-            { tabId: request.tabid },
-            "Page.captureScreenshot",
-            {
-                // format: request.data.format, png,jpeg,webp
-                clip: { x: rect.x, y: rect.y, width: rect.width, height: rect.height, scale: 1 },
-                captureBeyondViewPort: true
-            }
-        )
-        chrome.debugger.detach({ tabId: request.tabid }, () => {
-            console.log('detach ok');
-        });
-
-        sendResponse({ response: "listeher is successful" });
-        return;
+        chrome.debugger.attach({ tabId: request.tabid }, "1.3", async () => {
+            console.log("Attach debugger")
+            // デバッガ起動待機
+            await new Promise((resolve) => setTimeout(resolve, 500));
+            chrome.debugger.sendCommand(
+                { tabId: request.tabid },
+                "Page.captureScreenshot",
+                {
+                    // format: request.data.format, png,jpeg,webp
+                    clip: { x: rect.x, y: rect.y, width: rect.width, height: rect.height, scale: 1 },
+                    captureBeyondViewPort: true
+                },
+                (result) => {
+                    console.log(result)
+                    chrome.debugger.detach({ tabId: request.tabid }, () => {
+                        console.log('detach ok');
+                    });
+                }
+            )
+            sendResponse({ response: "listeher is successful" });
+            return;
+        }
+        );
     }
-);
+)

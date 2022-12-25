d3fca986579479626c348a904711bca29bce7a42
JPCC <120706915+JPCC-p@users.noreply.github.com>
2022-12-25 23:17:04 +0000

    Add: option.js

--- src/options.js
+++ src/options.js
@@ -1,10 +1,18 @@
 document.querySelectorAll(".color").forEach((elm) => {
     elm.addEventListener("click", (e) => {
         var options = {
-            Capture_Mode: e.target.innerText,
-            Capture_Format: "png"   // set
-        }
+            Capture_Mode: e.target.innerText
+        };
         chrome.storage.sync.set(options);
         document.querySelector("#msg").innerText = `Set option to ${options.Capture_Mode}.`;
+    });
+});
+document.querySelectorAll(".type").forEach((elem) => {
+    elem.addEventListener("click", (e) => {
+        var options = {
+            Capture_Format: e.target.innerText   // set
+        };
+        chrome.storage.sync.set(options);
+        document.querySelector("#msg2").innerText = `Set option to ${options.Capture_Format}.`;
     });
 });
